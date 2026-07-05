0626b5ab0402c963b3872c7038fc52c7fec2cd78
Ravindra-kandregula <144910821+Ravindra-kandregula@users.noreply.github.com>
2026-07-05 13:28:13 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+container_id=$(sudo docker ps | awk 'NR==2 {print $1}')
+sudo docker rm -f $container_id
+echo "Container deleted successfully"
